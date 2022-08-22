c6dff1d722375ad9f5267990ba5236673989cfc6
Brian H. Hough | brianhuff.eth <56947075+BrianHHough@users.noreply.github.com>
2022-08-22 17:17:12 -0400

    mid-commit mint an nft module

--- src/pages/grid.js
+++ src/pages/grid.js
@@ -1,5 +1,6 @@
 import React, { useState, useRef, useEffect } from "react";
 import '../App.css';
+import Dropzone, { useDropzone } from "react-dropzone";
 
 // Routing
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
@@ -9,59 +10,179 @@ import { Amplify } from 'aws-amplify';
 import awsExports from '../aws-exports';
 Amplify.configure(awsExports);
 
+
 function Grid() {
     const ref = useRef();
+    const [NFTImageSuccess, setNFTImageSuccess] = useState();
+    const [NFTImage, setNFTImage] = useState();
+    
 
-  useEffect(() => {
-    // const cnv=document.getElementById("MillionDollarHomepage")
-    const canvas = ref.current.getContext("2d");
-    // small units
-    for (var i = 5; i <= 1000; i = i + 5) {
-      // verticals
-      canvas.moveTo(i, 0);
-      canvas.lineTo(i, 1000);
-
-      // horizontals
-      canvas.moveTo(0, i);
-      canvas.lineTo(1000, i);
-
-      canvas.strokeStyle = "lightgrey";
-      canvas.lineWidth = "0.5";
-      canvas.stroke();
+    const NFTMetadataInputs = {
+        name: "",
+        description: "",
+        image: "",
+        }
+        const [NFT, setNFT] = useState(NFTMetadataInputs);
+        
+    function onChange(e) {
+        setNFTImage(() => ({
+            ...NFT, [e.target.value]: e.target.value
+        }))
     }
-  }, []);
+
+    
+
+    const userAddress = "0x123456790"
+
+    useEffect(() => {
+        // const cnv=document.getElementById("MillionDollarHomepage")
+        const canvas = ref.current.getContext("2d");
+        // small units
+        for (var i = 5; i <= 1000; i = i + 5) {
+        // verticals
+        canvas.moveTo(i, 0);
+        canvas.lineTo(i, 1000);
+
+        // horizontals
+        canvas.moveTo(0, i);
+        canvas.lineTo(1000, i);
+
+        canvas.strokeStyle = "lightgrey";
+        canvas.lineWidth = "0.1";
+        canvas.stroke();
+        }
+    }, []);
   return (
     <div className="App">
-      
         <h1>Million Dollar Homepage</h1>
-        <canvas
-            id="MillionDollarHomepage"
-            ref={ref}
-            className="Canvas"
-            width={1000}
-            height={1000}
-            // width={window.innerWidth}
-            // height={window.innerHeight}
-            onClick={(e) => {
-            alert(
-                // `TokenID: ${e.clientX * e.clientY} -- 
-                // X-Value: ${e.clientX - e.screenX}, 
-                // Y-Value: ${e.clientY}`
+        <div style={{
+            display: "flex",
+            left: "50%",
+            transform: "translateX(-50%)",
+            position: "absolute"
+        }}>
+            <canvas
+                id="MillionDollarHomepage"
+                ref={ref}
+                className="Canvas"
+                width={1000}
+                height={1000}
+                // width={window.innerWidth}
+                // height={window.innerHeight}
+                onClick={(e) => {
+                alert(
+                    // `TokenID: ${e.clientX * e.clientY} -- 
+                    // X-Value: ${e.clientX - e.screenX}, 
+                    // Y-Value: ${e.clientY}`
 
-                `x*y: ${e.clientX * e.clientY} -- 
-                X-Client: ${e.clientX}, 
-                X-Screen:${e.screenX},
-                Y-Value: ${e.clientY}`
+                    `x*y: ${e.clientX * e.clientY} -- 
+                    X-Client: ${e.clientX}, 
+                    X-Screen:${e.screenX},
+                    Y-Value: ${e.clientY}`
 
-                // `TokenID: ${e.screenX * e.screenY} --
-                // X-Value: ${e.screenX},
-                // Y-Value: ${e.screenY}`
-            );
-            // alert(e);
-            }}
-        />
-      
-    </div>
+                    // `TokenID: ${e.screenX * e.screenY} --
+                    // X-Value: ${e.screenX},
+                    // Y-Value: ${e.screenY}`
+                );
+                // alert(e);
+                }}
+            />
+            <div style={{
+                width: "400px",
+                height: "1000px",
+                textAlign: "left",
+                padding: "0px 0px 0px 50px"
+            }}>
+                <h2>Mint an NFT</h2>
+                <p>Drag and drop an image:</p>
+                <form>
+                    <Dropzone 
+                        accept={"image/jpeg, image/png, image/webp"}
+                        
+                        onDrop={(acceptedFiles) => {
+                            setNFTImageSuccess([])
+                            setNFTImage(
+                            acceptedFiles.map((upFile) => 
+                                Object.assign(upFile, {
+                                    preview: URL.createObjectURL(upFile)
+                                }))
+                            );
+                        }}
+                        name="NFTImage"
+                        multiple={false}
+                    >
+                    {({ getRootProps, getInputProps }) => ( 
+                    <div style={{
+                        width: "200px",
+                        height: "200px",
+                        outline: "2px solid black"
+                        }}
+                        {...getRootProps({ className: "dropzone"})}
+                    >
+                        <input {...getInputProps()} />
+                        {NFTImage?.map((upFile) => {
+                            return(
+                                <>
+                                    <div>
+                                        <img
+                                            src={upFile.preview}
+                                            key={upFile.name}
+                                            width="200px"
+                                            height="200px"
+                                            alt="Uploaded Image"
+                                        />
+                                    </div>
+                                </>
+                            )
+                        })}
+                    </div>
+                    )}
+                    </Dropzone>
+                <p>File uploaded locally: {NFTImage ? "True" : "False"}</p>
+                {console.log(NFTImage)}
+                {NFTImage?.map((upFile) => {
+                    return(
+                        <div>
+                            <image
+                                src={upFile.preview}
+                                key={upFile.name}
+                                width="400px"
+                                height="1000px"
+                                alt="Uploaded Image"
+                                style={{
+                                    zIndex: "9999"
+                                }}
+                            />
+                        </div>
+                    )
+                })}
+                <div style={{display: "inline-grid"}}>
+                    <input 
+                        name="name"
+                        placeholder="Name your NFT..."
+                        value={onChange}
+                    />
+
+                    <input 
+                        name="description"
+                        placeholder="Add a description..."
+                        value={onChange}
+                    />
+
+                    <input 
+                        name="name"
+                        value={userAddress}
+                    />
+
+                    <button type="submit">MINT</button>
+                </div>
+                </form>
+
+                {console.log(NFT)}
+
+                </div>
+            </div>
+        </div>
   );
 }
 
